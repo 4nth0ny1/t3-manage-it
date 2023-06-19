c6d8c730d9698eca26a5a1abe5aec73132bb3a13
Anthony Catullo <anthonycatullo@oatslam.local>
2023-06-18 22:14:48 -0500

    added delete icon to the sprint item

--- src/components/sprints/SprintItem.tsx
+++ src/components/sprints/SprintItem.tsx
@@ -1,4 +1,5 @@
 import type { Sprint } from "../../types";
+import { RiDeleteBin2Fill } from "react-icons/ri";
 
 type SprintProps = {
   sprint: Sprint;
@@ -7,5 +8,14 @@ type SprintProps = {
 export function SprintItem({ sprint }: SprintProps) {
   const { id, name, description, createdAt } = sprint;
 
-  return <li className="w-full border-b border-black">{name}</li>;
+  return (
+    <li className="w-full border-b border-black">
+      <div className="flex flex-row justify-between">
+        {name}
+        <button className="text-error">
+          <RiDeleteBin2Fill />
+        </button>
+      </div>
+    </li>
+  );
 }

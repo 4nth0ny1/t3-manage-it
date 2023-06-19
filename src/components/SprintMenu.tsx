8f25c56da4c0da97e32089a090c13a1739e5febd
Anthony Catullo <anthonycatullo@oatslam.local>
2023-06-18 22:08:16 -0500

    added icon for cancel create Sprint

--- src/components/SprintMenu.tsx
+++ src/components/SprintMenu.tsx
@@ -1,4 +1,4 @@
-import { GrAddCircle } from "react-icons/gr";
+import { GrAddCircle, GrSubtractCircle } from "react-icons/gr";
 import { useState } from "react";
 import { SprintList } from "../components/sprints/SprintList";
 import { CreateSprint } from "../components/sprints/CreateSprint";
@@ -11,7 +11,12 @@ export function SprintMenu() {
       <h2 className="text-2xl">Sprints</h2>
       <SprintList />
       {creating ? (
-        <CreateSprint resetCreating={() => setCreating(!creating)} />
+        <div>
+          <div className="flex flex-row justify-end py-4">
+            <GrSubtractCircle onClick={() => setCreating(!creating)} />
+          </div>
+          <CreateSprint resetCreating={() => setCreating(!creating)} />
+        </div>
       ) : (
         <div className="flex flex-row justify-end py-4">
           <GrAddCircle onClick={() => setCreating(!creating)} />

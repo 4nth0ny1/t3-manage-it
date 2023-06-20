3ff2674060175f2ac4ccc9b9904368625123ab3c
Anthony Catullo <anthonycatullo@oatslam.local>
2023-06-20 14:17:25 -0500

    added useEffect for sprintId on sprintId page

--- src/pages/project/sprint/[sprintId].tsx
+++ src/pages/project/sprint/[sprintId].tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { api } from "../../../utils/api";
 import { type NextPage } from "next";
 import { SprintMenu } from "~/components/SprintMenu";
@@ -7,7 +8,14 @@ import { Hero } from "~/components/Hero";
 
 const SingleSprintPage: NextPage = () => {
   const router = useRouter();
-  const sprintId = router.query.sprintId as string;
+
+  const [sprintId, setSprintId] = useState("");
+
+  useEffect(() => {
+    if (!router.isReady) return;
+    const sprintId = router.query.sprintId as string;
+    setSprintId(sprintId);
+  }, [router.isReady, router.query]);
 
   const {
     data: sprint,

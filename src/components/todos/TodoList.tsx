90989dcd316ca759950f63ffb65135ac059539d7
Anthony Catullo <anthonycatullo@oatslam.local>
2023-06-20 22:23:09 -0500

    added test text

--- src/components/todos/TodoList.tsx
+++ src/components/todos/TodoList.tsx
@@ -37,6 +37,7 @@ export function TodoList() {
         "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. ",
     },
   ];
+  /// test
 
   return (
     <ul className="flex flex-row flex-wrap text-2xl">

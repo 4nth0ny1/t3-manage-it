e7a45cce8d35a038e074f935ff21670d3a388d13
Anthony Catullo <anthonycatullo@oatslam.local>
2023-06-20 22:32:33 -0500

    added Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+
+export function Navbar() {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <Link href="/" className="btn-ghost btn text-xl normal-case">
+          <span className="text-[hsl(280,100%,70%)]">T3</span> Manage It
+        </Link>
+      </div>
+      <div className="flex-none gap-2">
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input-bordered input w-24 md:w-auto"
+          />
+        </div>
+        <div className="dropdown-end dropdown">
+          <label tabIndex={0} className="btn-ghost btn-circle avatar btn">
+            <div className="w-10 rounded-full">
+              <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu rounded-box menu-sm z-[1] mt-3 w-52 bg-base-100 p-2 shadow"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}

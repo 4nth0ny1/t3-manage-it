5a220f6ef1059cfefade77767239bda3528940f2
Anthony Catullo <anthonycatullo@oatslam.local>
2023-06-24 00:24:23 -0500

    only allow percent after sprint is chosen

--- src/components/todos/TodoList.tsx
+++ src/components/todos/TodoList.tsx
@@ -27,7 +27,7 @@ export function TodoList({ sprintId }: ProjectIdProps) {
     <div className="flex flex-row flex-wrap text-2xl">
       <div className="flex w-full flex-row justify-between px-20 py-10">
         <h2 className="text-3xl">{sprint?.name}</h2>
-        <ProgressBar percentDone={percentDone} />
+        {filteredList && <ProgressBar percentDone={percentDone} />}
       </div>
       {filteredList && (
         <div className="mb-6 flex w-full flex-row justify-center p-4">

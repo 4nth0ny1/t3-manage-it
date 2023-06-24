b78b54d0ad22a877fc1755858ec8e8b38c2ab604
Anthony Catullo <anthonycatullo@oatslam.local>
2023-06-23 20:01:11 -0500

    added sprint name to the todolist

--- src/components/todos/TodoList.tsx
+++ src/components/todos/TodoList.tsx
@@ -9,14 +9,13 @@ type ProjectIdProps = {
 };
 
 export function TodoList({ sprintId, projectId }: ProjectIdProps) {
+  const { data: sprint } = api.sprint.getOneSprint.useQuery({ sprintId });
+
   const {
     data: allTodos,
     isLoading,
     isError,
   } = api.todo.getAllTodos.useQuery({ sprintId });
-  //teest
-  // if (isLoading) return <div>Loading...</div>;
-  // if (isError) return <div>Something went wrong</div>;
 
   const filteredList = allTodos?.filter((todo) => {
     return sprintId === todo.sprintId;
@@ -24,6 +23,9 @@ export function TodoList({ sprintId, projectId }: ProjectIdProps) {
 
   return (
     <div className="flex flex-row flex-wrap text-2xl">
+      <div className="flex w-full flex-row justify-center p-10">
+        <h2 className="text-3xl">{sprint?.name}</h2>
+      </div>
       {filteredList && (
         <div className="mb-6 flex w-full flex-row justify-center p-4">
           <CreateTodo />

a30518427cd7bf68debdd69f3e9eedec5c50dbf8
Anthony Catullo <anthonycatullo@oatslam.local>
2023-06-25 14:57:15 -0500

    adding backspace icon

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,7 @@ import { EditProject } from "./projects/EditProject";
 import { AiFillEdit } from "react-icons/ai";
 import { useState } from "react";
 import ReactMarkdown from "react-markdown";
+import { BsBackspace } from "react-icons/bs";
 
 type ProjectProps = {
   id: string | undefined;
@@ -15,7 +16,6 @@ export function Hero({ id, name, description, projectId }: ProjectProps) {
   const [showDescription, setShowDescription] = useState(false);
 
   const shortDesc = description?.substring(0, 50);
-  //test    safsadfsasadfasafa
 
   return (
     <div className="hero flex flex-row justify-center gap-20 border-b bg-base-100">
@@ -53,10 +53,17 @@ export function Hero({ id, name, description, projectId }: ProjectProps) {
           </article>
         </div>
       </div>
-      <AiFillEdit
-        className="text-3xl text-green-400"
-        onClick={() => setProjectEditing(!editingProject)}
-      />
+      {editingProject ? (
+        <BsBackspace
+          className="text-3xl text-pink-400"
+          onClick={() => setProjectEditing(!editingProject)}
+        />
+      ) : (
+        <AiFillEdit
+          className="text-3xl text-green-400"
+          onClick={() => setProjectEditing(!editingProject)}
+        />
+      )}
       <div className="h-[400px] w-[600px]">
         {editingProject && (
           <EditProject

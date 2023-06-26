41f39881fadc1b2c27f7c386f5af121828e8a489
4nth0ny1 <acatullo4@gmail.com>
2023-06-26 17:12:46 -0500

    completed UnsignedPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { ProjectList } from "../components/projects/ProjectList";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
+import { UnsignedIndexPage } from "../components/UnsignedIndex";
 
 export default function Home() {
   const { data: sessionData } = useSession();
@@ -13,32 +14,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-8 ">
-          <h1 className="tracking-tightsm:text-[5rem] text-5xl font-extrabold">
-            <span className="text-[hsl(280,100%,70%)]">T3</span> Manage It
-          </h1>
-          <h2 className="text-2xl">Your Personal Project Manager</h2>
-        </div>
-        {sessionData ? <ProjectList /> : <AuthShowcase />}
+        {sessionData ? <ProjectList /> : <UnsignedIndexPage />}
       </main>
     </>
-  );
-}
-
-function AuthShowcase() {
-  const { data: sessionData } = useSession();
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl ">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-      </p>
-      <button
-        className="btn-accent btn rounded-full px-10 py-3 font-semibold no-underline transition"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
   );
 }

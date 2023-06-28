0ff222d064a6c6191097a57ccc2ee049bdee6277
4nth0ny1 <acatullo4@gmail.com>
2023-06-28 00:07:09 -0500

    added github auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -6,6 +6,8 @@ import {
   type DefaultSession,
 } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import GitHubProvider from "next-auth/providers/github";
+
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
 
@@ -51,6 +53,10 @@ export const authOptions: NextAuthOptions = {
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!
+    })
     /**
      * ...add more providers here.
      *

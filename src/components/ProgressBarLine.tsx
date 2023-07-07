3cb6416c8289a4c6bb008df573e95f3025c84a5b
4nth0ny1 <acatullo4@gmail.com>
2023-07-07 14:33:23 -0500

    changed color of progress bar

--- src/components/ProgressBarLine.tsx
+++ src/components/ProgressBarLine.tsx
@@ -5,7 +5,7 @@ type PercentProps = {
 export function ProgressBarLine({ percentDone }: PercentProps) {
   return (
     <progress
-      className="progress-info progress h-[30px] w-full border"
+      className="progress-accent progress h-[30px] w-full border"
       value={percentDone}
       max="100"
     ></progress>

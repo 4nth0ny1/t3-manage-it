f877135524de2da2ec525c9e66f15f48466bea19
Anthony C <44069058+4nth0ny1@users.noreply.github.com>
2023-07-26 00:10:46 -0500

    added styles and animations to the unsigned index page (#25)

--- src/components/UnsignedIndex.tsx
+++ src/components/UnsignedIndex.tsx
@@ -1,16 +1,39 @@
 import { signIn, signOut, useSession } from "next-auth/react";
+import { motion } from "framer-motion";
 
 export function UnsignedIndexPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-8 ">
-        <h1 className="tracking-tightsm:text-[5rem] text-5xl font-extrabold">
-          <span className="text-[hsl(280,100%,70%)]">T3</span> Manage It
-        </h1>
-        <h2 className="text-2xl">Your Personal Project Manager</h2>
+    <>
+      <motion.main
+        initial={{ opacity: 0, scale: 0.75 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ delay: 0.5, duration: 0.25 }}
+        className="z-10 mt-20 flex flex-col items-center rounded-xl border-2 px-20 py-10 shadow-2xl backdrop-blur-xl"
+      >
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-8 ">
+          <h1 className="tracking-tightsm:text-[5rem] text-5xl font-extrabold">
+            <span className="text-[hsl(280,100%,70%)]">T3</span> Manage It
+          </h1>
+          <h2 className="text-2xl">Your Personal Project Manager</h2>
+        </div>
+        <AuthShowcase />
+      </motion.main>
+      <div className="absolute top-96 z-0 w-full">
+        <div className="h-[75px] bg-primary"></div>
+        <div className="h-[75px] bg-accent"></div>
+        <div className="h-[75px] bg-secondary"></div>
       </div>
-      <AuthShowcase />
-    </main>
+      {/* <div className="z-10 w-[50%] py-80 text-center text-2xl text-white">
+        <p>
+          Manage It is a todo app that helps you stay organized with multiple
+          projects at the same time.
+        </p>
+        <p>
+          Manage It is a todo app that helps you stay organized with multiple
+          projects at the same time.
+        </p>
+      </div> */}
+    </>
   );
 }
 
